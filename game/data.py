88f9fa76e1ae82d357b3754bc9692ec4a7db103f
Martin Cejp <cejpmart@fel.cvut.cz>
2019-02-17 20:13:44 +0100

    How about some hexes?

--- game/data.py
+++ game/data.py
@@ -83,15 +83,39 @@ def csv_as_dict_of_mapped(f, map_, id_column):
 def csv_as_list_of_list(f):
     return [row for row in csv.reader(f)]
 
+# Input:  array of (rows, columns), stored in odd-Y staggered coordinates
+# Output: array of (r, q)
+def staggered_to_axial(tiles):
+    q_offset = int((tiles.shape[0] + 1) // 2)
+    out = np.zeros((q_offset + tiles.shape[1], tiles.shape[0]), dtype=tiles.dtype)
+
+    for row in range(tiles.shape[0]):
+        for col in range(tiles.shape[1]):
+            q = col - int((row - (row % 2)) // 2)
+            r = row
+            s = -r-q
+
+            assert q_offset + q >= 0
+            out[q_offset + q, r] = tiles[row, col]
+
+    return out
+
 def reload_data():
     global biomes, maps, tiletypes
 
     with open(basepath / "maps.csv", 'rt') as f:
         def map_from_csv(filename, name, entryx, entryy):
             with open(basepath / "maps" / (filename + "_biomes.csv"), 'rt') as f:
-                biomes = np.array(csv_as_list_of_list(f), dtype=np.uint8)
+                biomes = np.array(csv_as_list_of_list(f), dtype=np.int8)
             with open(basepath / "maps" / (filename + "_terrain.csv"), 'rt') as f:
-                terrain = np.array(csv_as_list_of_list(f), dtype=np.uint8)
+                terrain = np.array(csv_as_list_of_list(f), dtype=np.int8)
+
+            # fixme: use proper Tiled format
+            biomes[np.equal(biomes, -1)] = 0
+            terrain[np.equal(terrain, -1)] = 0
+
+            biomes = staggered_to_axial(biomes)
+            terrain = staggered_to_axial(terrain)
 
             return Map(name, (int(entryx), int(entryy)), biomes, terrain)
 
--- preprocess-tiles.py
+++ preprocess-tiles.py
@@ -9,18 +9,29 @@ studio_data_path = game.package_base_path / 'studio_data'
 biomes_png_path = studio_data_path / 'biomes'
 tiles_png_path = studio_data_path / 'tiles'
 
-png_size = 32
+png_size = (56, 64)
 
 tiletype_list = []
 
 for required_path in [biomes_png_path, studio_data_path, tiles_png_path]:
     required_path.mkdir(parents=True, exist_ok=True)
 
+def convert(from_, to, background='transparent', resize=None):
+    command = ['convert', '-background', background]
+
+    if resize:
+        command += ['-resize', '%dx%d' % resize]
+
+    command += [from_, to]
+    subprocess.check_call(command)
+
 def generate_blank_image(path, w, h, color):
     subprocess.check_call(['convert', '-size', f'{w}x{h}', 'canvas:' + color, path])
 
 for id, tiletype in data.tiletypes.items():
     print('preprocess', tiletype['name'])
+    # TODO: tiled's CSV format is retarded, we need to work around it / use something else
+
     src_file = game.package_base_path / 'static' / 'icons' / (tiletype['icon'] + '.svg')
     png32_file = tiles_png_path / (str(id) + '.png')
 
@@ -41,17 +52,39 @@ for id, tiletype in data.tiletypes.items():
             with open(dst_file, 'wt') as output:
                 output.write(svg.replace('#fff', fgcolor))
 
-        subprocess.check_call(['convert', '-resize', f'{png_size}x{png_size}', '-background', bgcolor, dst_file, png32_file])
+        subprocess.check_call(['convert', '-resize', f'{png_size[0]}x{png_size[1]}', '-background', bgcolor, dst_file, png32_file])
     else:
         # TODO: red-fill explicitly invalidated tile types
-        generate_blank_image(png32_file, png_size, png_size, bgcolor)
+        generate_blank_image(png32_file, png_size[0], png_size[1], bgcolor)
 
     tiletype_list.append((tiletype, png32_file))
+
+##### generate all needed hex backgrounds
+with open(game.package_base_path / 'static' / 'icons' / 'hex.svg', 'rt') as input:
+    svg = input.read()
+
+def generate_hex_by_colorname(colorname):
+    bgcolor = data.palette[colorname]
+    dst_file = game.package_base_path / 'static' / 'icons' / 'tiles' / f"hex.{colorname}.svg"
+
+    with open(dst_file, 'wt') as output:
+        output.write(svg.replace('#fff', bgcolor))
+
+for biome_id, biome in data.biomes.items():
+    for colorname in [biome['bgcolor'], biome['fgcolor']]:  # TODO: remove fgcolor from this list when definitely not needed
+        if colorname == '':
+            continue
+
+        generate_hex_by_colorname(colorname)
+
+generate_hex_by_colorname('black')
+
+#####
 
 with open(studio_data_path / 'biomes.tsx', 'wt') as tsx:
     tsx.write(f'''\
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="biomes" tilewidth="{png_size}" tileheight="{png_size}" tilecount="{len(tiletype_list)}" columns="0">
+<tileset name="biomes" tilewidth="{png_size[0]}" tileheight="{png_size[1]}" tilecount="{len(tiletype_list)}" columns="0">
   <grid orientation="orthogonal" width="1" height="1"/>
 ''')
 
@@ -60,14 +93,15 @@ with open(studio_data_path / 'biomes.tsx', 'wt') as tsx:
         assert id == i
 
         biome = data.biomes[id]
-        bgcolor = data.bgpalette[biome['bgcolor']]
+
+        colored_hex_svg = game.package_base_path / 'static' / 'icons' / 'tiles' / f"hex.{biome['bgcolor']}.svg"
 
         png = biomes_png_path / (str(id) + '.png')
-        generate_blank_image(png, png_size, png_size, bgcolor)
+        convert(colored_hex_svg, png, resize=png_size)
 
         tsx.write(f'''\
   <tile id="{i}" type="{biome['name']}">
-    <image width="{png_size}" height="{png_size}" source="{png}"/>
+    <image width="{png_size[0]}" height="{png_size[1]}" source="{png}"/>
   </tile>
 ''')
 
@@ -78,7 +112,7 @@ with open(studio_data_path / 'biomes.tsx', 'wt') as tsx:
 with open(studio_data_path / 'tileset.tsx', 'wt') as tsx:
     tsx.write(f'''\
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="terrain_tiles" tilewidth="{png_size}" tileheight="{png_size}" tilecount="{len(tiletype_list)}" columns="0">
+<tileset name="terrain_tiles" tilewidth="{png_size[0]}" tileheight="{png_size[1]}" tilecount="{len(tiletype_list)}" columns="0">
   <grid orientation="orthogonal" width="1" height="1"/>
 ''')
 
@@ -86,7 +120,7 @@ with open(studio_data_path / 'tileset.tsx', 'wt') as tsx:
     for i, (tiletype, png32_file) in enumerate(tiletype_list):
         tsx.write(f'''\
   <tile id="{i}" type="{tiletype['name']}">
-    <image width="{png_size}" height="{png_size}" source="{png32_file}"/>
+    <image width="{png_size[0]}" height="{png_size[1]}" source="{png32_file}"/>
   </tile>
 ''')
 
